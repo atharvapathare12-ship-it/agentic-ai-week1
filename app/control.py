9535163144e90f5fe8ca4587be451d6b2e48e9ee
Atharva Pathare <atharva@fucientllc.onmicrosoft.com>
2026-01-16 10:31:54 +0530

    Week 1 Day 2: modified control flow to allow multiple steps

--- app/control.py
+++ app/control.py
@@ -2,7 +2,7 @@ def decide_next_step(memory: dict) -> str:
     if memory["completed"]:
         return "stop"
 
-    if len(memory["steps"]) == 0:
+    if len(memory["steps"]) == 2:
         return "call_llm"
 
     memory["completed"] = True
